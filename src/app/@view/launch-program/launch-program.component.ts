a289048397b06948daea58a8b35a5c1f533f1c44
Shubham vijayvargiya <henceforthmac@HenceforthMacs-Mac-mini.local>
2020-09-22 03:45:06 +0530

    UI changes

--- src/app/@view/launch-program/launch-program.component.ts
+++ src/app/@view/launch-program/launch-program.component.ts
@@ -9,6 +9,27 @@ import { API_TYPE } from 'src/app/@core/@utills/api-type';
   styleUrls: ['./launch-program.component.scss']
 })
 export class LaunchProgramComponent implements OnInit {
+  years = [
+    {value:'2006', isSelected: false},
+    {value:'2007', isSelected: false},
+    {value:'2008', isSelected: false},
+    {value:'2009', isSelected: false},
+    {value:'2010', isSelected: false},
+    {value:'2011', isSelected: false},
+    {value:'2012', isSelected: false},
+    {value:'2013', isSelected: false},
+    {value:'2014', isSelected: false},
+    {value:'2015', isSelected: false},
+    {value:'2016', isSelected: false},
+    {value:'2017', isSelected: false},
+    {value:'2018', isSelected: false},
+    {value:'2019', isSelected: false},
+    {value:'2020', isSelected: false}
+  ];
+
+  launch_success: any;
+  land_success: any;
+  launchData: any;
 
   constructor(private cs : CommonService) { }
 
@@ -18,11 +39,17 @@ export class LaunchProgramComponent implements OnInit {
   }
 
   getLaunchPrograms(){
-    this.cs.httpRequest(API_TYPE.GET, 'launches', {
-      limit:100
-    }).subscribe(
+    let req : any = { limit:100 };
+
+    // if(this.launch_success) , land_success: this.land_success
+
+    req.launch_year = this.years.filter(item=>item.isSelected).map(item=>item.value)
+
+    this.cs.httpRequest(API_TYPE.GET, 'launches', req).subscribe(
       (res: any) => {
         console.log(res);
+
+        this.launchData = res;
         
       },
       (err : HttpErrorResponse)  => {
@@ -30,6 +57,16 @@ export class LaunchProgramComponent implements OnInit {
         
       }
     );
+  }
+
+  changeFilterYear(index){
+    for(let i=0;i<this.years.length;i++){
+      if(i == index)
+        this.years[i].isSelected = !this.years[i].isSelected;
+      else
+      this.years[i].isSelected = false;
+    }
+    this.getLaunchPrograms();
   }
 
 }
